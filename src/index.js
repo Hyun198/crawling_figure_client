0375d67a6ecb8f2668fd5b1d1853c4d101b85642
Hyun <hwn123h@naver.com>
2024-06-01 17:30:02 +0900

    local

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 import './index.css';
 import App from './App';
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'https://port-0-crawling-figure-server-1mrfs72llwvr46pf.sel5.cloudtype.app';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
